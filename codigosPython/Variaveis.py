88126be13fbca6afe125401d1d8a8f72c3caa95e
Ry4n-Ferreir4 <130564393+Ry4n-Ferreir4@users.noreply.github.com>
2023-05-27 18:11:30 +0000

    Atualização-02

---
+++ codigosPython/Variaveis.py
@@ -0,0 +1,18 @@
+
+#alunos = ['Yasmin', 'Ana', 'Bruno']
+#print(f"{alunos}, {notas}")
+
+
+
+
+#valores = (25, 36, 21)
+#print(valores[0])
+
+"""
+
+altura = {'joao': 1.73, 'Ana' : 1.60}
+print(altura)
+altura['pedro'] = 1.36
+print(altura)
+
+"""
